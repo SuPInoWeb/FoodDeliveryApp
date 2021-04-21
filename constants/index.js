eba3f12c9db45c5d8d421b3ffae845e139a01712
sudi@1998 <sudiksha@fangame.live>
2021-04-21 18:55:27 +0530

    commited

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import icons from "./icon";
+import images from "./image";
+import { COLORS, SIZES, FONTS } from "./theme";
+import GOOGLE_API_KEY from "./map"
+
+export { icons, images, COLORS, SIZES, FONTS, GOOGLE_API_KEY };
